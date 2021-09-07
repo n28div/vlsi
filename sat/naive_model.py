4581e1b47d6d8c8fe876dbde0a83cc068e2abc38
Jippiter <jipmaijers1@gmail.com>
2021-09-07 15:18:41 +0200

    restore change

--- sat/naive_model.py
+++ sat/naive_model.py
@@ -136,9 +136,10 @@ class NaiveModel(SatModel):
     
     for c in range(self.N):
       for i in range(self.HEIGHT):
-        for j in range(self.WIDTH):
-          constraints.append(z3.Implies(self.cy[c, i], self._exactly_n(self.cboard[c, i, :], self.cwidth[c])))
-          constraints.append(z3.Implies(self.cx[c, j], self._exactly_n(self.cboard[c, :, j], self.cheight[c])))
+        constraints.append(self._exactly_n(self.cy[c, i], 1))
+      for j in range(self.WIDTH):
+        constraints.append(self._exactly_n(self.cx[c, j], 1))
+
     
     return z3.And(constraints)
   
@@ -166,9 +167,9 @@ class NaiveModel(SatModel):
     Post constraints on the model
     """
     self.solver.add(
-      #self.cx_cy_leftbottom_constraint(),
+      self.cx_cy_leftbottom_constraint(),
       self.channeling_constraint(),
-      #self.bound_constraint(),
+      self.bound_constraint(),
       self.overlapping_constraint(),
-      #self.placement_constraint(),
+      self.placement_constraint(),
     )
